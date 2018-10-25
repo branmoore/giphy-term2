7f48e741e173e98ae927adfa251da2aef0592a95
Brandon Moore <branluciano@Brandons-MacBook-Air.local>
2018-10-24 17:26:04 -0700

    fixing

---
+++ checklist.py
@@ -0,0 +1,32 @@
+checklist = []
+
+# CREATE
+def create(item):
+    checklist.append(item)
+
+def read(index):
+    return checklist[index]
+
+# UPDATE
+def update(index, item):
+    checklist[index] = item
+
+    # DESTROY
+def destroy(index):
+    checklist.pop(index)
+
+def test():
+    # Add your testing code here
+
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+
+test()
